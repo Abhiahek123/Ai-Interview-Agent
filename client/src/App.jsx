5f635bcb4bbe99fc01d313854493d1e5bd6cdf34
Abhishek Singh <rajputabhishek9886@gmail.com>
2026-04-21 19:56:37 +0530

    fix case-sensitive import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Auth from "./pages/Auth" //change 
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/auth'
